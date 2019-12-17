3625295f2217142d39986a9db68b9630cfac4b90
Kevoki <57940305+Kevoki@users.noreply.github.com>
2019-12-17 09:39:22 +0300

    Added the calsulation and multiplication function

--- js/scripts.js
+++ js/scripts.js
@@ -1,102 +1,17 @@
-// var price , crust_price, topping_price ;
-// let total = 0;
-// function Getpizza( flavours,size,crusts,toppings,number ){
-//   this.flavours = flavours;
-//   this.size = size;
-//   this.crusts = crusts;
-//   this.toppings= toppings;
-//   this.number = number;
-// }
-
 $(document).ready(function() {
-  //$("button.proceed").click(function(){
-  //$("button.proceed").hide();
-  //$("#information").hide();
-  //$("div.choise").slideDown(1000);
-
-  
   $("#checkout").click(function(event){
     event.preventDefault();
-   let pflavour = $("#flavours option:selected").val();
-   let psize = $("#sizes option:selected").val();
-   let pcrust = $("#crusts option:selected").val();
-   let ptoppings = $("#topppings option:selected").val();
-   let ptopping = [];
-   $.each($("input[name='toppings']:checked"), function(){            
-       ptopping.push($(this).val());
-   });
-   console.log(ptopping.join(", "));
+   let pflavour = parseInt($("#flavours option:selected").val());
+   let psize = parseInt($("#sizes option:selected").val());
+   let pcrust = parseInt( $("#crusts option:selected").val());
+   let ptopping = parseInt( $("#toppings option:selected").val());
+   let pnumber = parseInt($("#number").val());
+ 
+   let total = pflavour + psize + pcrust + ptopping;
+   let grandTotal = total * pnumber;
+   
+   console.log(grandTotal)
 
-    let topping_value = ptopping.length*50;
-    console.log("toppins value" + topping_value);
-    if((psize == "0") && (pcrust == "0")){
-      console.log("nothing selected");
-      $("button.proceed").show();
-      $("#information").show();
-      $("div.choise").hide();
-      alert("Please select pizza size and crust"); 
-    }
-    else{
-      $("button.proceed").hide();
-      $("#information").hide();
-      $("div.choise").slideDown(1000);
-    }
-
-    total = pflavour + psize + pcrust + ptoppings;
-    console.log(total);
-    let checkoutTotal =0;
-    checkoutTotal = checkoutTotal + total;
-    $("#pizzaname").html($(".name option:selected").val());
-    $("#pizzasize").html( $("#size option:selected").val());
-    $("#pizzacrust").html($("#crust option:selected").val());
-    $("#pizzatopping").html(ptopping.join(", "));
-    $("#totals").html(total);
-    
-// Add pizza button
-    $("button.addPizza").click(function(){
-      let pname = $(".name option:selected").val();
-      let psize = $("#size option:selected").val();
-      let pcrust = $("#crust option:selected").val();
-      let ptopping = [];
-      $.each($("input[name='toppings']:checked"), function(){            
-          ptopping.push($(this).val());
-      });
-
-      console.log(ptopping.join(", "));
-
-        let topping_value = ptopping.length*50;
-        console.log("toppins value" + topping_value);
-        total = price + crust_price + topping_value;
-        console.log(total);
-        checkoutTotal = checkoutTotal + total;
-        console.log(checkoutTotal);
-      // constractor function
-      var newOrder = new Getpizza(pflavours, psize, pcrust,ptopping,total);
-      $("#ordersmade").append('<tr><td id="pizzaname">'+newOrder.name +'</td><td id="pizzasize">' + newOrder.size + '</td><td id="pizzacrust">'+newOrder.crust + '</td><td id="pizzatopping">'+newOrder.topping+'</td><td id="totals">'+newOrder.total+'</td></tr>');
-      console.log(newOrder);
-    });
-    // Checkout button
-    $("button#checkout").click(function(){ 
-      $("button#checkout").hide();
-      $("button.addPizza").hide();
-      $("button.deliver").slideDown(1000);
-      $("#addedprice").slideDown(1000);
-      console.log("Your total bills is sh. "+checkoutTotal);
-      $("#pizzatotal").append("Your bill is sh. "+checkoutTotal);
-    });
-
-    // home delivery button
-    $("button.deliver").click(function(){
-      $(".pizzatable").hide();
-      $(".choise h2").hide();
-      $(".delivery").slideDown(1000);
-      $("#addedprice").hide();
-      $("button.deliver").hide();
-      $("#pizzatotal").hide();
-      let deliceryamount= checkoutTotal+150;
-      console.log("You will pay sh. "+deliceryamount+" on delivery");
-      $("#totalbill").append("Your bill plus delivery fee is: "+deliceryamount);
-    });
    
   });
 });
