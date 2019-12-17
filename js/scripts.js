39137f529d335542c5fc60ac4d1d964b72fdd62e
Kevoki <57940305+Kevoki@users.noreply.github.com>
2019-12-17 10:03:24 +0300

    Added a checkout button

--- js/scripts.js
+++ js/scripts.js
@@ -22,7 +22,49 @@ $(document).ready(function() {
      '<td>'+ grandTotal+'</td>'+
       '</tr>');
 
-   
+
+      $(".checkout").click(function() {
+        $(".checkout").hide();
+        $(".delivery").show();
+        $("#yes").hide();
+        $("#no").hide();
+        $("#totalcost").show();
+        $("#delivercost").show();
+        $(".yes-button").show();
+        $(".no-button").show();
+        $(".address").hide();
+        grandTotal = grandTotal + total;
+        $("p span").html(grandTotal);
+      });
+      $(".yes-button").click(function() {
+      $("#delivercost").hide();
+      $("#no").hide();
+      $("#yes").hide();
+      $(".address").show();
+      $(".yes-button").hide();
+      $(".no-button").hide();
+      $("#totalcost span").html(grandTotal + 300);
+    });
+      $(".no-button").click(function() {
+      $("#delivercost").hide();
+      $("#yes").hide();
+      $("#no").show();
+      $(".yes-button").hide();
+      $(".no-button").hide();
+      $("#totalcost span").html(grandTotal);
+    });
+    $("#addaddress").click(function() {
+    var address = $(".address input").val();
+    $("#yes").show();
+    $(".address").hide();
+    $("#yes span").html(address);
+  });
+  $(".refresh").click(function(event){
+    $('form').each(function(){
+      this.reset();
+      $(".delivery").hide();
+      $("#tablecart tbody tr").remove();
+    }); 
   });
 });
 
