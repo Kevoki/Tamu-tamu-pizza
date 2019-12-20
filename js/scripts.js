65e6dc7b4cb7a6fb14d622c05cae5b6623d1b644
Kevoki <57940305+Kevoki@users.noreply.github.com>
2019-12-20 07:56:29 +0300

    Used prototypes and constructors in the functions to calculate the price of pizza'

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,19 @@
-$(document).ready(function() {
+// Business logic
+function PizzaOrder(flavour, size, crust, topping, number) {
+  this.flavour = flavour
+  this.size = size
+  this.crust = crust
+  this.topping = topping
+  this.number = number
+}
+
+PizzaOrder.prototype.pizzaPrice = function () {
+  let total = (this.flavour + this.size + this.crust + this.topping);
+  return total;
+}
+
+//User interface logic
+$(document).ready(function () {
   $("#yes").hide();
   $("#no").hide();
   $("#totalcost").hide();
@@ -6,46 +21,47 @@ $(document).ready(function() {
   $(".yes-button").hide();
   $(".no-button").hide();
   $(".address").hide();
-  
 
 
-  $("#order").click(function(event){
+  $("#order").click(function (event) {
     event.preventDefault();
-   let pflavour = parseInt($("#flavours option:selected").val());
-   let psize = parseInt($("#sizes option:selected").val());
-   let pcrust = parseInt( $("#crusts option:selected").val());
-   let ptopping = parseInt( $("#toppings option:selected").val());
-   let pnumber = parseInt($("#number").val());
- 
-   let total = pflavour + psize + pcrust + ptopping;
-   let grandTotal = total * pnumber;
-   
-   console.log(grandTotal)
 
-   $('table#tablecart').append(
-     '<tr>'+
-     '<td>'+ $("#flavours option:selected").text()+'</td>'+
-     '<td>'+ $("#sizes option:selected").text()+'</td>'+
-     '<td>'+ $("#crusts option:selected").text()+'</td>'+
-     '<td>'+ $("#toppings option:selected").text()+'</td>'+
-     '<td>'+ pnumber+'</td>'+
-     '<td>'+ grandTotal+'</td>'+
+    let pflavour = parseInt($("#flavours option:selected").val());
+    let psize = parseInt($("#sizes option:selected").val());
+    let pcrust = parseInt($("#crusts option:selected").val());
+    let ptopping = parseInt($("#toppings option:selected").val());
+    let pnumber = parseInt($("#number").val());
+
+    let newOrder = new PizzaOrder(pflavour, psize, pcrust, ptopping, number)
+    let total = newOrder.pizzaPrice();
+    let grandTotal = total * newOrder.number;
+
+    console.log(grandTotal)
+
+    $('table#tablecart').append(
+      '<tr>' +
+      '<td>' + $("#flavours option:selected").text() + '</td>' +
+      '<td>' + $("#sizes option:selected").text() + '</td>' +
+      '<td>' + $("#crusts option:selected").text() + '</td>' +
+      '<td>' + $("#toppings option:selected").text() + '</td>' +
+      '<td>' + pnumber + '</td>' +
+      '<td>' + grandTotal + '</td>' +
       '</tr>');
 
-      $(".checkout").click(function() {
-        $(".checkout").hide();
-        $(".delivery").show();
-        $("#yes").hide();
-        $("#no").hide();
-        $("#totalcost").show();
-        $("#delivercost").show();
-        $(".yes-button").show();
-        $(".no-button").show();
-        $(".address").hide();
-        grandTotal = grandTotal + total;
-        $("p span").html(grandTotal);
-      });
-      $(".yes-button").click(function() {
+    $(".checkout").click(function () {
+      $(".checkout").hide();
+      $(".delivery").show();
+      $("#yes").hide();
+      $("#no").hide();
+      $("#totalcost").show();
+      $("#delivercost").show();
+      $(".yes-button").show();
+      $(".no-button").show();
+      $(".address").hide();
+      grandTotal = grandTotal + total;
+      $("p span").html(grandTotal);
+    });
+    $(".yes-button").click(function () {
       $("#delivercost").hide();
       $("#no").hide();
       $("#yes").hide();
@@ -54,7 +70,7 @@ $(document).ready(function() {
       $(".no-button").hide();
       $("#totalcost span").html(grandTotal + 100);
     });
-      $(".no-button").click(function() {
+    $(".no-button").click(function () {
       $("#delivercost").hide();
       $("#yes").hide();
       $("#no").show();
@@ -62,19 +78,20 @@ $(document).ready(function() {
       $(".no-button").hide();
       $("#totalcost span").html(grandTotal);
     });
-    $("#addaddress").click(function() {
-    var address = $(".address input").val();
-    $("#yes").show();
-    $(".address").hide();
-    $("#yes span").html(address);
-  });
-  $(".refresh").click(function(event){
-    $('form').each(function(){
-      this.reset();
-      $(".delivery").hide();
-      $("#tablecart tbody tr").remove();
-    }); 
+    $("#addaddress").click(function () {
+      var address = $(".address input").val();
+      $("#yes").show();
+      $(".address").hide();
+      $("#yes span").html(address);
+    });
+    $(".refresh").click(function (event) {
+      $('form').each(function () {
+        this.reset();
+        $(".delivery").hide();
+        $("#tablecart tbody tr").remove();
+      });
+    });
   });
 });
-});
+
 
